c9040620c11d172031b37a980a6b4f8993cdffcb
Alireza <arnn379@gmail.com>
2022-05-31 00:10:53 +0430

    stable commit

--- network.cpp
+++ network.cpp
@@ -26,8 +26,8 @@ std::vector<std::string> split (std::string str);
 
 void remove_edge(std::vector<std::string> command);
 void show_topology();
-void distance_vector();
-void link_state();
+void distance_vector(std::vector<std::string> command);
+void link_state(std::vector<std::string> command);
 void update_edge(std::vector<std::string> command);
 void create_edge(std::vector<std::string> command);
 void extend_nodes(int new_number_of_nodes);
@@ -39,8 +39,8 @@ void extend_nodes(int new_number_of_nodes);
 int main(){
 	std::string command;
 	
-	while (getline(std::cin,command)){
-		std::vector<std::string> splited_command= split(command);
+	while (getline(std::cin,command)) {
+		std::vector<std::string> splited_command = split(command);
 
 		if (splited_command.size() > 0){
 			if (splited_command[0] == "topology") {
@@ -50,10 +50,10 @@ int main(){
 				show_topology();
 			}
 			else if (splited_command[0] == "lsrp") {
-				link_state();
+				link_state(splited_command);
 			}
 			else if (splited_command[0] == "dvrp") {
-				distance_vector();
+				distance_vector(splited_command);
 			}
 			else if (splited_command[0] == "modify") {
 				update_edge(splited_command);
@@ -173,11 +173,11 @@ void belman_ford(int n,std::vector< std::vector<int> >& adj,
 }
 */
 std::vector<std::string> split (std::string str) {  
-	std::string word= "";
+	std::string word = "";
 	std::vector<std::string> splited_string;
 	for (char letter: str){
 		if (is_delimiter(letter)){
-			if (word!= ""){
+			if (word!= "") {
 				splited_string.push_back(word);
 			}
 			word= "";
@@ -186,7 +186,7 @@ std::vector<std::string> split (std::string str) {
 			word+= letter;
 		}
 	}
-	if (word!= "")
+	if (word != "")
 		splited_string.push_back(word);
 	
 	return splited_string;
@@ -197,13 +197,13 @@ bool is_delimiter(char charachter){
 }
 
 void create_edge(std::vector<std::string> command){
-	for (int i= 1; i<command.size(); i+=3){
-		int source_node= std::stoi(command[i])-1;
-		int destination_node= std::stoi(command[i+1])-1;
-		int cost_of_edge= std::stoi(command[i+2]);
+	for (int i= 1; i<command.size(); i += 3){
+		int source_node= std::stoi(command[i]) - 1;
+		int destination_node= std::stoi(command[i + 1]) - 1;
+		int cost_of_edge= std::stoi(command[i + 2]);
 		if (source_node== destination_node){
 			std::cout<< "Source and Destibation are same! node: "+ 
-				std::to_string(source_node+1)+ '\n';
+				std::to_string(source_node + 1) + '\n';
 			continue;
 		}
 		if (source_node>= number_of_nodes){
@@ -218,7 +218,7 @@ void create_edge(std::vector<std::string> command){
 		adj[destination_node].push_back(source_node);
 		weight[destination_node][source_node]= cost_of_edge;
 
-		for (int i=0; i< number_of_nodes; i++){
+		for (int i = 0; i < number_of_nodes; i++){
 			weight[i][i]= 0;
 		}
 	}
@@ -244,11 +244,11 @@ void show_topology(){
 }
 
 void extend_nodes(int new_number_of_nodes){
-	if (new_number_of_nodes>= number_of_nodes){
-		int prev_number_of_nodes= number_of_nodes;
-		while(new_number_of_nodes>= number_of_nodes){
+	if (new_number_of_nodes >= number_of_nodes){
+		int prev_number_of_nodes = number_of_nodes;
+		while(new_number_of_nodes >= number_of_nodes){
 			adj.push_back(std::vector<int>());
-			weight.push_back(std::vector<int> (prev_number_of_nodes,-1));
+			weight.push_back(std::vector<int> (prev_number_of_nodes, -1));
 			number_of_nodes++;
 		}
 		for (int i=0;i<number_of_nodes; i++){
@@ -271,8 +271,10 @@ void remove_edge(std::vector<std::string> command) {
 	weight[destination_node][source_node]= -1;
 }
 
-void distance_vector(){}
-void link_state(){}
+void distance_vector(std::vector<std::string> command) {}
+
+void link_state(std::vector<std::string> command) {}
+
 void update_edge(std::vector<std::string> command) {
 	int source_node = std::stoi(command[1]) - 1;
 	int destination_node = std::stoi(command[2]) - 1;
@@ -286,6 +288,5 @@ void update_edge(std::vector<std::string> command) {
 		
 	}
 	weight[source_node][destination_node] = new_weight;
-	weight[destination_node][source_node] = new_weight;
-		
+	weight[destination_node][source_node] = new_weight;	
 }
