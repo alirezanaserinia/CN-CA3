bd6c3cd4852baca8ffee4d2dd0491d0c14ee77a4
Alireza <arnn379@gmail.com>
2022-06-02 17:50:45 +0430

    lsrp added & reformat file

--- network.cpp
+++ network.cpp
@@ -4,37 +4,38 @@
 #include <climits>
 #include <string>
 #include <algorithm>
-
-// struct edge_tuple
-// {
-// 	int u,v;
-// 	edge_tuple(int _u, int _v){
-// 		u= _u;
-// 		v= _v;
-// 	};
-// 	bool operator==(const edge_tuple & e){
-// 		return (e.u==u && e.v==v);
-// 	}
-// } typedef edge;
+#include <set>
 
 int number_of_nodes = 1;
 std::vector< std::vector<int> > adj(1);
 std::vector< std::vector<int> > weight(1,std::vector<int> (1,-1));
 
-void print_path(int source, int i, int* parent);
-int find_next_hop(int source, int i,int* parent);
 bool is_delimiter(char charachter);
 std::vector<std::string> split (std::string str);
 
-void remove_edge(std::vector<std::string> command);
-void show_topology();
-void distance_vector(std::vector<std::string> command);
-void link_state(std::vector<std::string> command);
-void update_edge(std::vector<std::string> command);
-void create_edge(std::vector<std::string> command);
 void extend_nodes(int new_number_of_nodes);
-void print_distance_vector_by_source(int source);
+void create_edge(std::vector<std::string> command);
+
+void show_topology();
+
+void update_edge(std::vector<std::string> command);
+
+void remove_edge(std::vector<std::string> command);
+
+// Link State
+void link_state(std::vector<std::string> command);
+void print_lsrp_by_source(int source_node);
+void dijkstra(int source, long int* dist, int* parent);
+void print_iteration(int &iter, long int* dist);
+void print_lsrp_table(int source, long int* dist, int* parent);
+void print_path_lsrp(int source, int i, int* parent);
+
+// Distance Vector
+void distance_vector(std::vector<std::string> command);
+void print_dvrp_by_source(int source);
 void belman_ford(int source,long int* dist,int* parent);
+int find_next_hop(int source, int i,int* parent);
+void print_path_dvrp(int source, int i, int* parent);
 
 
 int main() {
@@ -70,28 +71,8 @@ int main() {
 	return 0;
 }
 
-void belman_ford(int source,long int* dist,int* parent){
-
-	for (int i=0; i< number_of_nodes; i++){
-		dist[i]= INT_MAX;
-		parent[i]= -1;
-	}
-
-	dist[source]=0;
-
-	for (int k=0; k<number_of_nodes;k++){
-		for (int i=0; i<number_of_nodes; i++){
-			for (int j=0; j<adj[i].size();j++){
-				int u=i, v= adj[i][j], edge_weight= weight[u][v];
-				if (weight[u][v]== -1)
-					edge_weight= INT_MAX;
-				if (dist[v]> dist[u]+ edge_weight){
-					dist[v]= dist[u]+ edge_weight;
-					parent[v]= u;
-				}
-			}
-		}
-	}
+bool is_delimiter(char charachter) {
+	return charachter == ' ' || charachter == '-';
 }
 
 std::vector<std::string> split(std::string str) {  
@@ -114,8 +95,19 @@ std::vector<std::string> split(std::string str) {
 	return splited_string;
 } 
 
-bool is_delimiter(char charachter) {
-	return charachter == ' ' || charachter == '-';
+void extend_nodes(int new_number_of_nodes) {
+	if (new_number_of_nodes >= number_of_nodes) {
+		int prev_number_of_nodes = number_of_nodes;
+		while(new_number_of_nodes >= number_of_nodes) {
+			adj.push_back(std::vector<int>());
+			weight.push_back(std::vector<int> (prev_number_of_nodes, -1));
+			number_of_nodes++;
+		}
+		for (int i = 0; i < number_of_nodes; i++) {
+			for (int j = prev_number_of_nodes; j < number_of_nodes; j++)
+				weight[i].push_back(-1);
+		}
+	}
 }
 
 void create_edge(std::vector<std::string> command) {
@@ -165,108 +157,6 @@ void show_topology() {
 	}	
 }
 
-void extend_nodes(int new_number_of_nodes) {
-	if (new_number_of_nodes >= number_of_nodes){
-		int prev_number_of_nodes = number_of_nodes;
-		while(new_number_of_nodes >= number_of_nodes){
-			adj.push_back(std::vector<int>());
-			weight.push_back(std::vector<int> (prev_number_of_nodes, -1));
-			number_of_nodes++;
-		}
-		for (int i = 0; i < number_of_nodes; i++){
-			for (int j = prev_number_of_nodes; j < number_of_nodes; j++)
-				weight[i].push_back(-1);
-		}
-	}
-}
-
-void remove_edge(std::vector<std::string> command) {
-	int source_node = std::stoi(command[1]) - 1;
-	int destination_node = std::stoi(command[2]) - 1;
-	if (weight[source_node][destination_node] == -1)
-		return;
-	adj[source_node].erase(std::remove(adj[source_node].begin(),
-	adj[source_node].end(), destination_node), adj[source_node].end());
-	adj[destination_node].erase(std::remove(adj[destination_node].begin(),
-	adj[destination_node].end(), source_node), adj[destination_node].end());
-	weight[source_node][destination_node] = -1;
-	weight[destination_node][source_node] = -1;
-}
-
-void print_path(int source, int i, int* parent) {
-	std::cout << '[';
-	if (i == source) {
-		std::cout << i + 1;
-		std::cout <<']';
-		return;
-	}
-	std::vector<int> path_leaf_to_root;
-	while(parent[i] != source) {
-		path_leaf_to_root.push_back(i);
-		i = parent[i];
-	}
-	path_leaf_to_root.push_back(i);
-	std::cout << source + 1;
-	for (int i = path_leaf_to_root.size() - 1; i >= 0; i--) {
-		std::cout << "->" + std::to_string(path_leaf_to_root[i] + 1);
-	}
-	std::cout << ']';
-}
-
-int find_next_hop(int source, int i, int* parent) {
-	if (i == source){
-		return i;
-	}
-	while(parent[i] != source){
-		i = parent[i];
-	}
-	return i;
-}
-void print_distance_vector_by_source(int source_node) {
-	long int* dist = new long int [number_of_nodes];
-	int* parent = new int [number_of_nodes];
-	// long int dist[number_of_nodes];
-	// int parent[number_of_nodes];
-	belman_ford(source_node, dist, parent);
-	std::cout << '\n';
-	std::cout << "Dest\tNext Hop\tDist\tShorterst path\n";
-	std::cout << "----------------------------------------------\n";
-	for (int i = 0; i < number_of_nodes; i++){
-		std::cout << i + 1 << '\t';
-		int next_hop = find_next_hop(source_node, i, parent) + 1;
-		std::cout << next_hop << "\t\t";
-		std::cout << dist[i] << '\t';
-		print_path(source_node, i, parent);
-		std::cout << '\n';
-	}
-}
-void distance_vector(std::vector<std::string> command){
-	if (command.size() == 2){
-		int source_node = std::stoi(command[1]) - 1;
-		print_distance_vector_by_source(source_node);
-	}
-	else if (command.size() == 1){
-		for (int source_node = 0; source_node < number_of_nodes; source_node++){
-			print_distance_vector_by_source(source_node);
-		}
-	}
-	else {
-		std::cout << "invalid input!\n";
-	}
-}
-void link_state(std::vector<std::string> command) {
-	if (command.size() == 2) {
-		int source_node = std::stoi(command[1]) - 1;
-	}
-	else if (command.size() == 1) {
-		
-		
-	}
-	else {
-		std::cout << "invalid input!\n";
-	}
-}
-
 void update_edge(std::vector<std::string> command) {
 	int source_node = std::stoi(command[1]) - 1;
 	int destination_node = std::stoi(command[2]) - 1;
@@ -282,3 +172,232 @@ void update_edge(std::vector<std::string> command) {
 	weight[source_node][destination_node] = new_weight;
 	weight[destination_node][source_node] = new_weight;	
 }
+
+void remove_edge(std::vector<std::string> command) {
+	int source_node = std::stoi(command[1]) - 1;
+	int destination_node = std::stoi(command[2]) - 1;
+	if (weight[source_node][destination_node] == -1)
+		return;
+	adj[source_node].erase(std::remove(adj[source_node].begin(),
+	adj[source_node].end(), destination_node), adj[source_node].end());
+	adj[destination_node].erase(std::remove(adj[destination_node].begin(),
+	adj[destination_node].end(), source_node), adj[destination_node].end());
+	weight[source_node][destination_node] = -1;
+	weight[destination_node][source_node] = -1;
+}
+
+// Link State
+void print_path_lsrp(int source, int i, int* parent) {
+
+	std::vector<int> path_leaf_to_root;
+	while(parent[i] != source) {
+		path_leaf_to_root.push_back(i);
+		i = parent[i];
+	}
+	path_leaf_to_root.push_back(i);
+	std::cout << source + 1;
+	for (int i = path_leaf_to_root.size() - 1; i >= 0; i--) {
+		std::cout << " --> " + std::to_string(path_leaf_to_root[i] + 1);
+	}
+	std::cout << "\n";
+}
+
+void print_lsrp_table(int source, long int* dist, int* parent) {
+	std::cout << "Path:\t[s] --> [d]\tMin-Cost\tShortest Path\n";
+	std::cout << "\t---------\t---------\t-------------\n";
+	for (int i = 0; i < number_of_nodes; i++) {
+		if (i == source)
+			continue;
+		else {
+		std::cout << "\t[" << source + 1 << "] --> [" << i + 1 << "]\t" << dist[i] << "\t\t";
+		
+
+		
+
+		print_path_lsrp(source, i , parent); 
+		}
+	}
+}
+
+void print_iteration(int &iter, long int* dist) {
+	std::cout << "\t\titer " << iter << ":" << std::endl;
+	std::cout << "Dest\t\t| ";
+	for (int i = 0; i < number_of_nodes; i++) {
+		std::cout << i + 1 << "|\t";
+	}
+	std::cout << "\nCost\t\t| ";
+	for (int i = 0; i < number_of_nodes; i++) {
+		if (dist[i] == INT_MAX)
+			std::cout << -1 << "|\t";
+		else
+			std::cout << dist[i] << "|\t";
+	}
+	std::cout << "\n";
+	std::cout << "----------------------------------------------\n";
+}
+
+void dijkstra(int source, long int* dist, int* parent) {
+	for (int i = 0; i < number_of_nodes; i++){
+		dist[i] = INT_MAX;
+		parent[i] = -1;
+	}
+	dist[source] = 0;
+	std::set<int> S, V, V_S;
+	S.insert(source);
+
+	for (int i = 0; i < number_of_nodes; i++) {
+		if (i != source)
+			V_S.insert(i);
+		V.insert(i);
+	}
+	
+	for (int i = 0; i < adj[source].size(); i++) {
+		dist[adj[source][i]] = weight[source][adj[source][i]];
+		parent[adj[source][i]] = source;
+	}
+
+	int arg_min, min, u, v, edge_weight, iter = 1 ;
+	while (S != V)	{
+		print_iteration(iter, dist); 
+		iter++;
+		arg_min = 0, min = INT_MAX;
+		for (auto k = V_S.begin(); k != V_S.end(); k++) {
+			if (dist[*k] < min) {
+				min = dist[*k];
+				arg_min = *k;
+			}
+		}
+		u = arg_min;
+		S.insert(u);
+		V_S.erase(u);
+
+		for (int i = 0; i < adj[u].size(); i++) {
+			if (V_S.find(adj[u][i]) != V_S.end()) {
+				v = adj[u][i];
+				edge_weight = weight[u][v];
+
+				/////////////////////
+				// std::cout << min << "|" << u + 1 << " " << v + 1 << "|" << edge_weight << "&\n";
+				
+				
+				if (weight[u][v] == -1)
+					edge_weight = INT_MAX;
+				if (dist[v] > dist[u] + edge_weight) {
+					dist[v] = dist[u] + edge_weight;
+					parent[v] = u;
+					
+				}
+			}
+		}
+	}
+}
+
+void print_lsrp_by_source(int source_node) {
+	long int* dist = new long int [number_of_nodes];
+	int* parent = new int [number_of_nodes];
+	dijkstra(source_node, dist, parent);
+	print_lsrp_table(source_node, dist, parent);	
+}
+
+void link_state(std::vector<std::string> command) {
+	if (command.size() == 2) {
+		int source_node = std::stoi(command[1]) - 1;
+		print_lsrp_by_source(source_node);
+
+	}
+	else if (command.size() == 1) {
+		
+		
+	}
+	else {
+		std::cout << "invalid input!\n";
+	}
+}
+
+// Distance Vector
+void print_path_dvrp(int source, int i, int* parent) {
+	std::cout << '[';
+	if (i == source) {
+		std::cout << i + 1;
+		std::cout <<']';
+		return;
+	}
+	std::vector<int> path_leaf_to_root;
+	while(parent[i] != source) {
+		path_leaf_to_root.push_back(i);
+		i = parent[i];
+	}
+	path_leaf_to_root.push_back(i);
+	std::cout << source + 1;
+	for (int i = path_leaf_to_root.size() - 1; i >= 0; i--) {
+		std::cout << " --> " + std::to_string(path_leaf_to_root[i] + 1);
+	}
+	std::cout << ']';
+}
+
+int find_next_hop(int source, int i, int* parent) {
+	if (i == source){
+		return i;
+	}
+	while(parent[i] != source){
+		i = parent[i];
+	}
+	return i;
+}
+
+void belman_ford(int source, long int* dist, int* parent){
+	for (int i = 0; i < number_of_nodes; i++){
+		dist[i] = INT_MAX;
+		parent[i] = -1;
+	}
+	dist[source] = 0;
+	for (int k = 0; k < number_of_nodes; k++){
+		for (int i = 0; i < number_of_nodes; i++){
+			for (int j = 0; j < adj[i].size(); j++){
+				int u = i, v = adj[i][j], edge_weight = weight[u][v];
+				if (weight[u][v] == -1)
+					edge_weight = INT_MAX;
+				if (dist[v] > dist[u] + edge_weight) {
+					dist[v] = dist[u] + edge_weight;
+					parent[v] = u;
+				}
+			}
+		}
+	}
+}
+
+void print_dvrp_by_source(int source_node) {
+	long int* dist = new long int [number_of_nodes];
+	int* parent = new int [number_of_nodes];
+	// long int dist[number_of_nodes];
+	// int parent[number_of_nodes];
+	belman_ford(source_node, dist, parent);
+	std::cout << '\n';
+	std::cout << "Dest\tNext Hop\tDist\tShorterst path\n";
+	std::cout << "----------------------------------------------\n";
+	for (int i = 0; i < number_of_nodes; i++){
+		std::cout << i + 1 << '\t';
+		int next_hop = find_next_hop(source_node, i, parent) + 1;
+		std::cout << next_hop << "\t\t";
+		std::cout << dist[i] << '\t';
+		print_path_dvrp(source_node, i, parent);
+		std::cout << '\n';
+	}
+}
+
+void distance_vector(std::vector<std::string> command) {
+	if (command.size() == 2){
+		int source_node = std::stoi(command[1]) - 1;
+		print_dvrp_by_source(source_node);
+	}
+	else if (command.size() == 1){
+		for (int source_node = 0; source_node < number_of_nodes; source_node++){
+			print_dvrp_by_source(source_node);
+		}
+	}
+	else {
+		std::cout << "invalid input!\n";
+	}
+}
+
+
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+td::cout << "\n///////////////////////////\n";
+	// for (int i = 0; i < number_of_nodes; i++)
+	// 	std::cout << parent[i] + 1 << " ";
+	// std::cout << "\n";
